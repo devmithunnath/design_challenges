ff35172dfce1d9b1777cea78c7373fb3c6ef97f1
Mithun Nath <mithun.nath@ecompliance.com>
2020-03-20 23:25:19 -0400

    card styles

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -4,30 +4,44 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: false,
+        title: Text(
+          "Design Challenges",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
       body: ListView(
         children: <Widget>[
           CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
-          ),CardHolder(
+          ),
+          CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
-          ),CardHolder(
+          ),
+          CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
-          ),CardHolder(
+          ),
+          CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
-          ),CardHolder(
+          ),
+          CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
-          ),CardHolder(
+          ),
+          CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
-          ),CardHolder(
+          ),
+          CardHolder(
             title: "Newspaper",
             subTitle: "Simple and Elegant",
           ),
--- lib/widgets/cardHolder.dart
+++ lib/widgets/cardHolder.dart
@@ -3,12 +3,21 @@ import 'package:flutter/material.dart';
 class CardHolder extends StatelessWidget {
   final String title;
   final String subTitle;
+  
   CardHolder({@required this.title, @required this.subTitle});
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      elevation: 0,
+      margin: EdgeInsets.all(0),
+      borderOnForeground: false,
+      shape: ContinuousRectangleBorder(
+        borderRadius: BorderRadius.circular(0),
+      ),
       child: ListTile(
+        onTap: (){
+
+        },
         title: Text(title ?? "Unavailable"),
         subtitle: Text(subTitle ?? "Unavailable"),
       ),
